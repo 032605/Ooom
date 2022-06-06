50a0642382b41effa2fd9c2f3f9a7bd3022b4dc7
032605 <032605@naver.com>
2022-06-06 17:47:26 +0900

    sending a message between front and back

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server💫");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("New Message : ", message.data, " from the server.");
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnected from Server💢");
+});
+
+setTimeout(() => {
+    socket.send("Time Out!");
+}, 10000);
